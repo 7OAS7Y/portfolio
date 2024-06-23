0bcbf676d1ee8ae6653f813454f94b8cfa7098b1
Joshua Crothers <joshcrothers1@gmail.com>
2024-06-23 19:40:59 -0400

    added nav button component

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,19 +1,6 @@
-import { useRef } from 'react'
-import { gsap } from 'gsap'
-import { useGSAP } from '@gsap/react'
 import { Box } from '@mui/system'
 
-gsap.registerPlugin(useGSAP);
-
 const About = () => {
-
-  const about = useRef();
-  
-  useGSAP(() => {
-    // gsap code here...
-    gsap.to(".bio", {rotation: 180}); // <-- automatically reverted
-  
-  }, { scope: about }) // <-- scope for selector text (optional)
 
   return (
     <Box 
@@ -24,10 +11,10 @@ const About = () => {
     borderRadius={3}
     gap={4}
     p={2}
-    backgroundColor="#8b9ec966"
+    backgroundColor="#cee3e782"
     sx={{ border: '2px solid #5c7282', textAlign: 'center', maxWidth: '800px', boxShadow: '0px 10px 100px 7px #324673e0' }}>
-        <div ref={about} className='about'>
-            <p id='bio'>Passionate and detail-oriented Front-End Developer with over 3 years of professional experience in designing, developing, and maintaining responsive websites and web applications. Proficient in HTML, CSS, JavaScript, and modern frameworks such as React. Proven ability to translate design concepts into functional, user-friendly digital experiences.</p>
+        <div className='about'>
+          <p>Passionate and detail-oriented Front-End Developer with over 3 years of professional experience in designing, developing, and maintaining responsive websites and web applications. Proficient in HTML, CSS, JavaScript, and modern frameworks such as React. Proven ability to translate design concepts into functional, user-friendly digital experiences.</p>
         </div>
     </Box>
   )
--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,11 +1,23 @@
 import React from 'react'
+import { Box } from '@mui/system'
 
 const Contact = () => {
   return (
-    <div>
-        <h1>Contact</h1>
-        <p>This is more about us.</p>
-    </div>
+    <Box 
+    height={800}
+    m="0 8px"
+    display="flex"
+    alignItems="center"
+    borderRadius={3}
+    gap={4}
+    p={2}
+    backgroundColor="#cee3e782"
+    sx={{ border: '2px solid #5c7282', textAlign: 'center', maxWidth: '800px', boxShadow: '0px 10px 100px 7px #324673e0' }}>
+      <div className='contact'>
+          <h1>Contact</h1>
+          <p>This is more about us.</p>
+      </div>
+    </Box>
   )
 }
 
